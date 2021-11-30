24b3afdc0ef8849351777ec37c90ceabd50f8618
Tabakovic Amar <amar.tabakovic@students.bfh.ch>
2021-11-30 10:37:17 +0100

    Comments

--- amar-cat.c
+++ amar-cat.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * @brief Reads and prints the content of a file with a given file name.
+ * 
+ * @param file_name file name
+ */
 void read_contents(char *file_name)
 {
 	FILE *file_pointer;
@@ -22,6 +27,12 @@ void read_contents(char *file_name)
 	fclose(file_pointer);
 }
 
+/**
+ * @brief Checks whether a file with a given file name exists.
+ * 
+ * @param file_name file name
+ * @return int 0 if the file exists, -1 if the file doesn't exist
+ */
 int check_file_exists(char* file_name)
 {
 	FILE *file_pointer;
@@ -40,6 +51,13 @@ int check_file_exists(char* file_name)
 	return 0;
 }
 
+/**
+ * @brief Entry point of the application.
+ * 
+ * @param argc number of command line arguments
+ * @param argv command line arguments 
+ * @return int exit code
+ */
 int main(int argc, char **argv) 
 {
 	if(2 > argc)
